67f8a0578cbb87f4ae65feb5c83d156b5b8067b0
rkrauskopf <randall.krauskopf@gmail.com>
2022-03-12 09:09:59 -0600

    math.

--- public/app.js
+++ public/app.js
@@ -3,15 +3,43 @@
 const form = document.getElementById("rate-form");
 const resultDisplay = document.getElementById("result");
 
+
+const formulationMap = {
+  "standard": {
+    "3": 32,
+    "3.7": 26,
+    "4": 24,
+    "4.17": 24,
+    "4.5": 22,
+    "5": 20,
+  },
+  "1.5x": {
+    "3": 48,
+    "3.7": 39,
+    "4": 36,
+    "4.17": 34,
+    "4.5": 32,
+    "5": 30,
+  },
+  "2x": {
+    "3": 64,
+    "3.7": 52,
+    "4": 48,
+    "4.17": 48,
+    "4.5": 44,
+    "5": 40,
+  },
+}
+
+
 document.querySelector("#rate-form").addEventListener("submit", function(e){
     e.preventDefault();    //stop form from submitting
     const price = Number(form.price.value);
     const gallons = Number(form.gallons.value);
-    const app_rate = Number(form["application-rate"].value);
-    const formulation = Number(form.formulation.value);
+    const app_rate = form["application-rate"].value;
+    const formulation = form.formulation.value;
 
+    const result = (price * gallons * formulationMap[app_rate][formulation])/32;
 
-    const result = (price * gallons * app_rate * formulation)/32;
-
-    resultDisplay.textContent = result;
+    resultDisplay.textContent = result.toFixed(2);
 });
