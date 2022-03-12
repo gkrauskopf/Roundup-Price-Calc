e6adbd651e0c8aea0fabaf4651e8bb66586af275
rkrauskopf <randall.krauskopf@gmail.com>
2022-03-12 08:48:28 -0600

    additional stuff

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// const connectBtn = document.getElementById("connect");
+
+const form = document.getElementById("rate-form");
+const resultDisplay = document.getElementById("result");
+
+document.querySelector("#rate-form").addEventListener("submit", function(e){
+    e.preventDefault();    //stop form from submitting
+    const price = Number(form.price.value);
+    const gallons = Number(form.gallons.value);
+    const app_rate = Number(form["application-rate"].value);
+    const formulation = Number(form.formulation.value);
+
+
+    const result = (price * gallons * app_rate * formulation)/32;
+
+    resultDisplay.textContent = result;
+});
